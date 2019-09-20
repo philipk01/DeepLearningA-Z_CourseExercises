0f4f7d701b9ca275dff778a214fe7831dc6753c3
philipk01 <philipk01@gmail.com>
2019-09-19 19:39:20 -0700

    external variable prediction

--- CustomerRetentionPrediction.py
+++ CustomerRetentionPrediction.py
@@ -85,6 +85,24 @@ cm = confusion_matrix(y_test, y_pred)
 print("Model accuracy: ", round((1491+225)/(2000), 3)*100, '%')
 
 
+"""
+predict if the customer with the following informations will leave the bank: 
+
+    Geography: France
+    Credit Score: 600
+    Gender: Male
+    Age: 40 years old
+    Tenure: 3 years
+    Balance: $60000
+    Number of Products: 2
+    Does this customer have a credit card ? Yes
+    Is this customer an Active Member: Yes
+    Estimated Salary: $50000
+    So should we say goodbye to that customer ?
+
+"""
+
+
 new_prediction = classifier.predict(sc.fit_transform(np.array([[0, 0, 600, 1,  40, 3, 60000, 2, 1, 1, 50000]])))
 new_prediction = (new_prediction > 0.5)
 
