502b293ca8ee932905e1bb3e92a6ec64e6d90001
Philip Khoury <philipk01@gmail.com>
2020-04-16 11:10:22 -0400

    keras preprocessing image

--- CNN_Image_Classification/CNN_ic_model.py
+++ CNN_Image_Classification/CNN_ic_model.py
@@ -18,34 +18,19 @@ from keras.preprocessing import image
 from keras import backend
 from keras.callbacks import Callback
 
+file = 'C:\\Users\\phili\\main\\git\\DeepLearningA-Z_HandsOnCourse\\CNN_Image_Classification\\'
 
-class LossHistory(Callback):
-    def __init__(self):
-        super().__init__()
-        self.epoch_id = 0
-        self.losses = ''
- 
-    def on_epoch_end(self, epoch, logs={}):
-        self.losses += "Epoch {}: accuracy -> {:.4f}, val_accuracy -> {:.4f}\n"\
-            .format(str(self.epoch_id), logs.get('acc'), logs.get('val_acc'))
-        self.epoch_id += 1
- 
-    def on_train_begin(self, logs={}):
-        self.losses += 'Training begins...\n'
+script_dir = os.path.dirname(file)
+# script_dir = os.path.dirname(__file__)
+training_set_path = os.path.join(script_dir, 'data\\training_set')
+test_set_path = os.path.join(script_dir, 'data\\test_set')
 
-file = 'C:\Users\phili\EDU\Deep_Learning\Deep_Learning_A_Z\Volume\ 1\ -\ Supervised\ Deep\ Learning\Part\ 2\ -\ Convolutional\ Neural\ Networks\ (CNN)'
-
-script_dir = os.path.dirname(__file__)
-training_set_path = os.path.join(script_dir, '../dataset/training_set')
-test_set_path = os.path.join(script_dir, '../dataset/test_set')
- 
 # Initialising the CNN
 classifier = Sequential()
  
 # Step 1 - Convolution
-input_size = (128, 128)
+input_size = (64, 64)
 classifier.add(Conv2D(32, (3, 3), input_shape=(*input_size, 3), activation='relu'))
- 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size=(2, 2)))  # 2x2 is optimal
  
@@ -61,13 +46,17 @@ classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Flatten())
  
 # Step 4 - Full connection
-classifier.add(Dense(units=64, activation='relu'))
-classifier.add(Dropout(0.5))
+classifier.add(Dense(units=128, activation='relu'))
+# classifier.add(Dropout(0.1))
 classifier.add(Dense(units=1, activation='sigmoid'))
  
 # Compiling the CNN
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
- 
+
+
+############################
+#    Image Preprocessing 
+############################
 # Part 2 - Fitting the CNN to the images
 batch_size = 32
 train_datagen = ImageDataGenerator(rescale=1. / 255,
@@ -76,40 +65,51 @@ train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    horizontal_flip=True)
  
 test_datagen = ImageDataGenerator(rescale=1. / 255)
- 
+    
 training_set = train_datagen.flow_from_directory(training_set_path,
                                                  target_size=input_size,
                                                  batch_size=batch_size,
                                                  class_mode='binary')
- 
+
+training_set.class_indices
+
 test_set = test_datagen.flow_from_directory(test_set_path,
                                             target_size=input_size,
                                             batch_size=batch_size,
                                             class_mode='binary')
- 
-# Create a loss history
-history = LossHistory()
- 
+
 classifier.fit_generator(training_set,
-                         steps_per_epoch=8000/batch_size,
-                         epochs=90,
-                         validation_data=test_set,
-                         validation_steps=2000/batch_size,
-                         workers=12,
-                         max_q_size=100,
-                         callbacks=[history])
- 
- 
+                         steps_per_epoch = 8000, # num images in training set
+                         epochs = 1,
+                         validation_data = test_set,
+                         validation_steps = 2000) # num images in test set
+
 # Save model
-model_backup_path = os.path.join(script_dir, '../dataset/cat_or_dogs_model.h5')
+model_backup_path = os.path.join(script_dir, 'cat_or_dogs_model.h5')
 classifier.save(model_backup_path)
 print("Model saved to", model_backup_path)
- 
-# Save loss history to file
-loss_history_path = os.path.join(script_dir, '../loss_history.log')
-myFile = open(loss_history_path, 'w+')
-myFile.write(history.losses)
-myFile.close()
- 
-backend.clear_session()
-print("The model class indices are:", training_set.class_indices)
+
+# preprocess image
+import numpy as np
+from keras.preprocessing import image
+img = os.path.join(script_dir, r'data\\single_prediction\\cat_or_dog_1.jpg')
+test_img = image.load_img(img, target_size = input_size)
+test_img = image.img_to_array(test_img) # array of 3 dims
+# expand dim by 1, batch dim as required by algorithm
+test_img = np.expand_dims(test_img, axis = 0)
+
+# Load model
+from keras.models import load_model
+model = load_model('cat_or_dogs_model.h5')
+
+# prediction
+result = model.predict(test_img)
+
+result[0][0]
+
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+print("The model predicted the image to be a", prediction)
