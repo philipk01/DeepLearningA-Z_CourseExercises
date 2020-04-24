546bebdd6c11437faeccf7472827ee48a28f7626
Philip Khoury <philipk01@gmail.com>
2020-04-24 16:06:30 -0400

    added ANN model to get probability of an anomaly for each input vector

--- Self_Organizing_Maps/anamoly_detection_fraud_hybrid_model_.py
+++ Self_Organizing_Maps/anamoly_detection_fraud_hybrid_model_.py
@@ -61,10 +61,10 @@ show()
 mappings = som.win_map(X) # dictionary
 
 # to get fraudulent accounts, get coordinates of white square from SOM map above
-# frauds = mappings[(7, 2)]
+frauds = mappings[(1, 3)]
 
-# concatenate when more than one outlier detected in SOM
-frauds = np.concatenate((mappings[2 ,1], mappings[3, 1], mappings[(4, 2)], mappings[(8, 8)]), axis = 0)
+# # concatenate when more than one outlier detected in SOM
+# frauds = np.concatenate((mappings[2 ,1], mappings[3, 1], mappings[(4, 2)], mappings[(8, 8)]), axis = 0)
 
 frauds = sc.inverse_transform(frauds)
 
@@ -72,10 +72,55 @@ frauds = sc.inverse_transform(frauds)
 # Part 2 - Unupervised --> Supervised deep learning
 # create dependant variable for the supervised model - ANN
 ###########################################################
-fid = frauds[:, 0:1].reshape(-1) # CustomerID of Frauds
-# fid = fid.reshape(-1)
+# # get df with fraudulent accounts only
+# fid = frauds[:, 0:1].reshape(-1) # CustomerID of Frauds
+# # fid = fid.reshape(-1)
+# df = dataset.sort_values(by = ['CustomerID'], ascending=True)
+# df_frauds = df[df['CustomerID'].isin(fid)]
 
-df = dataset.sort_values(by = ['CustomerID'], ascending=True)
 
-df_frauds = df[df['CustomerID'].isin(fid)]
+# Creating the matrix of features
+customers = dataset.iloc[:, 1:].values
+
+# create dependant variable
+is_fraud = np.zeros(len(dataset))
+
+for i in range(len(dataset)):
+    if dataset.iloc[i, 0] in frauds:
+        is_fraud[i] = 1
+
+        
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customers = sc.fit_transform(customers)
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = customers.shape[1]))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
+
+# Predicting the probabilities of fraud
+y_pred = classifier.predict(customers)
+
+# better to include CustomerID in y_pred
+y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
+
+# sort by predicted probability for cheating
+y_pred = y_pred[y_pred[:, 1].argsort()]
 
