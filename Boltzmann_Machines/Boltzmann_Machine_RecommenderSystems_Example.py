68418dae53df7af6f9fef89b531f8a200fd5d445
Philip Khoury <philipk01@gmail.com>
2020-04-27 12:01:37 -0400

    data preprocessing

--- Boltzmann_Machines/Boltzmann_Machine_RecommenderSystems_Example.py
+++ Boltzmann_Machines/Boltzmann_Machine_RecommenderSystems_Example.py
@@ -6,8 +6,9 @@ Created on Mon Apr 27 08:11:06 2020
 
 
 Data: MovieLens from GroupLens (https://grouplens.org/datasets/movielens/)
-
-
+we need 2 data direcotries:
+    1. ml-1m
+    2. ml-100k
 
 Question:
 Predict upward or downward trend for the stock price for 01/2017
@@ -30,3 +31,29 @@ movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'py
 users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
 ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
 
+# Preparing the training set and the test set - k-test fold left for autoencoders, hence only 1 set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype = 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
+
+# Getting the number of users and movies
+nb_users = int(max(max(training_set[:,0]), max(test_set[:,0])))
+nb_movies = int(max(max(training_set[:,1]), max(test_set[:,1])))
+
+# Converting the data into an array with users in lines and movies in columns
+def convert(data):
+    new_data = [] # to become a list of lists - a list for each user
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:,1][data[:,0] == id_users]
+        id_ratings = data[:,2][data[:,0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+training_set = convert(training_set)
+test_set = convert(test_set)
+
+# Converting the data into Torch tensors
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
