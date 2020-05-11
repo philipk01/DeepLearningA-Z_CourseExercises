1ffd80b64f1430f82091568788ac10682bb6f975
Nezo <philipk01@gmail.com>
2020-05-11 16:29:57 -0400

    optimized test For loop

--- AutoEncoders/AutoEncoders_RecommenderSystems_Example.py
+++ AutoEncoders/AutoEncoders_RecommenderSystems_Example.py
@@ -120,12 +120,13 @@ s = 0.
 for id_user in range(nb_users):
     input = Variable(training_set[id_user]).unsqueeze(0)
     target = Variable(test_set[id_user]).unsqueeze(0)
+    temp = torch.sum(target.data > 0)
     if torch.sum(target.data > 0) > 0:
         output = sae(input)
         target.require_grad = False
         output[target == 0] = 0
         loss = criterion(output, target)
-        mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+        mean_corrector = nb_movies/float(temp + 1e-10)
         test_loss += np.sqrt(loss.item() * mean_corrector)
         s += 1.
 print('test loss: '+str(test_loss/s))
